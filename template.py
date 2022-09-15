c054baf37eed927a1604b6cebcb5971df825269f
monk3yd <jegajardog@gmail.com>
2022-09-15 01:13:41 -0300

    Create python main template with execution timer

---
+++ template.py
@@ -0,0 +1,11 @@
+import time
+
+
+def main():
+    ...
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    main()
+    finish = time.perf_counter()
+    print(f'Finished in {round(finish-start, 2)} second(s)')
